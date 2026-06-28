b48f318070d053fc463f688bc68f756e064e3c7f
Joao Vitor Maximo <joaomaximo22@hotmail.com>
2026-06-28 20:22:45 -0300

    feat: add clear_screen driver

--- OBC/kernel.c
+++ OBC/kernel.c
@@ -1,5 +1,32 @@
+
+// Definicoes do tamanho da tela -> 25 colunas por 80 linhas = 2000
+#define SCREEN_WIDTH 25
+#define SCREEN_HEIGHT 80
+#define VIDEO_ADDRESS 0xB8000 // Endereço inicial de video
+
+int cursor_x;
+int cursor_y;
+
+void clear_screen()
+{
+
+    char *video_memory = (char *)VIDEO_ADDRESS; // Cria um ponteiro para o endereco de memora
+    char attribute_byte = 0x0F;                 // Cor preta com fundo branco
+
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) // percorre todos os enderecos aplicando o espcao em branco e fundo preto
+    {
+        video_memory[i * 2] = ' ';
+        video_memory[i * 2 + 1] = attribute_byte;
+    }
+
+    // Posicao inicial para o cursor
+    cursor_x = 0;
+    cursor_y = 0;
+}
+
 void kernel_main()
 {
+    clear_screen();
 
     char *video_memory = (char *)0xB8000;
 
