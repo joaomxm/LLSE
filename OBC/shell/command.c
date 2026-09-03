957dc09fc39c0b44ef9ee99f404e607cb43a700f
Joao Vitor Maximo <joaomaximo22@hotmail.com>
2026-09-03 20:49:18 -0300

    feat: add stat command

--- OBC/shell/command.c
+++ OBC/shell/command.c
@@ -9,7 +9,9 @@ static int get_command_count();
 static command_entry_t command_table[] = {
     {"ping", cmd_ping, "Verifica conectividade com o computador de bordo"},
     {"clear", cmd_clear, "Realiza a limpeza do terminal"},
-    {"help", cmd_help, "Detalhes dos comandos disponiveis"}};
+    {"help", cmd_help, "Detalhes dos comandos disponiveis"},
+    {"stat", cmd_stat, "Telemetria"},
+};
 
 void cmd_ping(int argc, char **args)
 {
@@ -31,6 +33,57 @@ void cmd_help()
     {
         printf("'%s' - (%s)\n", command_table[i].name, command_table[i].help);
     }
+}
+
+void cmd_stat()
+{
+    Eletrical_Power_System eletrical_power_system = {
+        +12.4, // V
+        +1.8,  // A
+        +18.5, // Celsius
+    };
+
+    ADCS adcs = {
+        NADIR,
+        0.01,  // X: rad/s,
+        -0.02, // Y: rad/s,
+        0.00,  // Z: rad/s
+        STANDBY,
+    };
+
+    OBC obc = {
+        "00h 42m 15s",
+        12.5,  // Percentage
+        16384, // bytes
+        65536, // bytes
+        3,
+        NOMINAL_MODE,
+    };
+
+    Thermal_Subsystem thermal_subsystem = {
+        +35.2, // Celsius
+        +28.0, // Celsius};
+    };
+
+    printf("=============================================\n");
+    printf("[OBC TELEMETRY REPORT - SAT_UNIT_01]\n");
+    printf("=============================================\n");
+    printf("[SYS] Uptime         : %s\n", obc.uptime);
+    printf("[SYS] Mode           : %s\n", Operation_Mode_text[obc.mode]);
+    printf("[SYS] Boot Count     : %d\n", obc.boot_count);
+    printf("---------------------------------------------\n");
+    printf("[EPS] Bus Voltage    : %f V\n", eletrical_power_system.bus_voltage);
+    printf("[EPS] Solar Current  : %f A\n", eletrical_power_system.solar_array_current);
+    printf("[EPS] Bat Temp       : %f C\n", eletrical_power_system.battery_temperature);
+    printf("---------------------------------------------\n");
+    printf("[ADCS] Orientation   : %s\n", Orientation_Mode_text[adcs.orientation_mode]);
+    printf("[ADCS] Gyro X/Y/Z    : %f / %f / %f\n", adcs.gyro_x, adcs.gyro_y, adcs.gyros_z);
+    printf("[ADCS] Magnetorquers : %s\n", Magnetorquers_Status_text[adcs.magnetorquers_status]);
+    printf("---------------------------------------------\n");
+    printf("[THERMAL] OBC Temp   : %f C\n", thermal_subsystem.cpu_temperature);
+    printf("[THERMAL] RF Temp    : %f C\n", thermal_subsystem.transceptor_rf_temperature);
+    printf("=============================================\n");
+    printf("[STATUS: ALL SUBSYSTEMS NOMINAL]\n");
 }
 
 static int get_command_count()
--- OBC/shell/command.h
+++ OBC/shell/command.h
@@ -11,9 +11,79 @@ typedef struct
     const char *help;
 } command_entry_t;
 
+typedef enum
+{
+    SAFE_MODE,    //: Desativa comandos pesados, mantém apenas a comunicação básica ligada (modo de economia de energia).
+    NOMINAL_MODE, //: Operação padrão; todos os comandos respondem normalmente.
+    PAYLOAD_MODE, //: Modo de coleta/execução de tarefas pesadas.
+} Operation_Mode;
+
+typedef enum
+{
+    NADIR,
+    SUN_TRACKING,
+    SPIN
+} Orientation_Mode;
+
+typedef enum
+{
+    STANDBY,
+    ACTIVE
+} Magnetorquers_Status;
+
+typedef struct
+{
+    float bus_voltage;
+    float solar_array_current;
+    float battery_temperature
+} Eletrical_Power_System;
+
+typedef struct
+{
+    Orientation_Mode orientation_mode;
+    float gyro_x;
+    float gyro_y;
+    float gyros_z;
+    Magnetorquers_Status magnetorquers_status
+} ADCS;
+
+typedef struct
+{
+    char *uptime;
+    float cpu_load;
+    int usage_heap_memory;
+    int total_heap_memory;
+    int boot_count;
+    Operation_Mode mode;
+} OBC;
+
+typedef struct
+{
+    float cpu_temperature;
+    float transceptor_rf_temperature;
+} Thermal_Subsystem;
+
+const char *Operation_Mode_text[] = {
+    "SAFE_MODE",
+    "NOMINAL_MODE",
+    "PAYLOAD_MODE",
+};
+
+const char *Orientation_Mode_text[] = {
+    "NADIR",
+    "SUN_TRACKING",
+    "SPIN",
+};
+const char *Magnetorquers_Status_text[] = {
+    "STANDBY",
+    "ACTIVE",
+
+};
+
 void cmd_ping(int argc, char **args);
 void cmd_clear();
 void cmd_help();
+void cmd_stat();
 void command_execute(char **args);
 
 #endif
